497b80d984440820e4903dbb8838be69f38d11d5
khadmsymper <khadm@symper.vn>
2020-04-09 11:14:25 +0700

    update'

---
+++ src/components/common/Notification.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-snackbar
+        v-model="snackbar"
+        :color="color"
+        :right="true"
+        :timeout="timeout"
+        :top="true"
+    >
+        {{ text }}
+        <v-btn dark text @click="snackbar = false">Close</v-btn>
+    </v-snackbar>
+</template>
+<script>
+export default {
+    name: 'notifications',
+    methods: {
+         
+    },
+    created() {
+        console.log(Vue,'xxxxxxxxxxxxxxxxx');        
+    },
+    props:{
+        timeout: {
+            default: 5000
+        },
+        color: {
+            default: 'info'
+        },
+        color: {
+            default: 'info'
+        },
+    },
+    data(){
+        return {
+            snackbar: false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/configs.js
+++ src/configs.js
@@ -2,5 +2,11 @@ export const appConfigs = {
     apiDomain: {
         user: 'https://v2hoangnd.dev.symper.vn/api/',
         bpmne: ''
+    },
+    notificationTimeout:{
+        success: 10000,
+        warning: 10000,
+        info: 10000,
+        error: -1,
     }
 };
