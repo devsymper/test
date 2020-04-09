f591d14ce7e8a8d04cbfb503f4fa00757101860a
khadmsymper <khadm@symper.vn>
2020-04-09 15:27:18 +0700

    kết hợp với firebase mess

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="symper-app">
         <component :is="layout">
-            <router-view/>
+            <router-view />
         </component>
     </div>
 </template>
@@ -13,17 +13,71 @@
 import AppSidebar from "./components/common/AppSidebar.vue";
 import Content from "./components/common/Content.vue";
 import appWorker from "@/worker";
+import { appConfigs } from "./configs.js";
+var firebase = require("firebase/app");
+require("firebase/messaging");
+const firebaseConfig = appConfigs.firebaseConfig;
 
 export default {
-    computed:{
-        layout(){
-            let isBA = this.$store.state.app.accountType == 'ba';
-            let contentOnly = this.$route.meta && this.$route.meta.layout == 'content-only'
+    created() {
+        this.initFirebase();
+    },
+    methods: {
+        initFirebase() {
+            var app = firebase.initializeApp(firebaseConfig);
+            var messaging = firebase.messaging();
+            messaging.usePublicVapidKey(
+                "BDDoFdEPGfIDi9fLXTJrCpiIBhok5xZmEKt0GHSVB7MrQz2xRCYWD6yu75dqRhxoXPQYFFfXdbzbhy0pp7E5YOI"
+            );
 
-            if(contentOnly){
-                return 'content-only-view';
-            }else{
-                return isBA ? 'ba-view' : 'end-user-view';
+            messaging.onMessage(payload => {
+                this.$evtBus.$emit(
+                    "app-receive-remote-msg",
+                    payload.data,
+                    payload.notification
+                );
+            });
+
+            messaging
+                .getToken()
+                .then(currentToken => {
+                    if (currentToken) {
+                        console.log(currentToken, "currentToken firebase");
+                    } else {
+                        console.log(
+                            "No Instance ID token available. Request permission to generate one."
+                        );
+                    }
+                })
+                .catch(err => {
+                    console.log(
+                        "An error occurred while retrieving token. ",
+                        err
+                    );
+                });
+                
+            messaging.onTokenRefresh(() => {
+                messaging
+                    .getToken()
+                    .then(refreshedToken => {
+                        console.log("Token refreshed.", refreshedToken);
+                    })
+                    .catch(err => {
+                        console.log("Unable to retrieve refreshed token ", err);
+                    });
+            });
+        }
+    },
+    computed: {
+        layout() {
+            let isBA = this.$store.state.app.accountType == "ba";
+            let contentOnly =
+                this.$route.meta && this.$route.meta.layout == "content-only";
+
+            if (contentOnly) {
+                return "content-only-view";
+            } else {
+                return isBA ? "ba-view" : "end-user-view";
             }
         }
     }
--- src/configs.js
+++ src/configs.js
@@ -8,5 +8,14 @@ export const appConfigs = {
         warning: 10000,
         info: 10000,
         error: -1,
+    },
+    firebaseConfig: {
+        apiKey: "AIzaSyCWalwPSvfi8GbdofE_whZMHLi_Fc0l7DE",
+        authDomain: "symper-f0ede.firebaseapp.com",
+        databaseURL: "https://symper-f0ede.firebaseio.com",
+        projectId: "symper-f0ede",
+        storageBucket: "symper-f0ede.appspot.com",
+        messagingSenderId: "315052954103",
+        appId: "1:315052954103:web:08b4ceaff3d03f0ad8def2"
     }
 };
