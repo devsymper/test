91d93b7e6767b94db22058b714d7e06b5521f5ba
khadmsymper <khadm@symper.vn>
2020-04-16 13:28:04 +0700

    sửa lỗi luôn cache request của api

--- src/api/BPMNEngine.js
+++ src/api/BPMNEngine.js
@@ -1,12 +1,12 @@
-import Api from "./api";
-import appConfigs from "./../configs.js";
+import Api from "./api"; // import class api vào để sử dụng
+import appConfigs from "./../configs.js"; // trong trường hợp này ta cần sử dụng domain của từng module nghiệp vụ được định nghĩa trong file config
 
-var bpmneApi = new Api(appConfigs.bpmne);
+var bpmneApi = new Api(appConfigs.bpmne); // Khởi tạo một đối tượng api với domain của service BPMNE
 export default {
     /** 
      * Lấy danh sách các process đã được tạo ra
      */
     getListProcess() {
-        return bpmneApi.get("process");
+        return bpmneApi.get("process"); //sử dụng đối tượng api để gọi API có method GET, với resource là process
     }
 };
