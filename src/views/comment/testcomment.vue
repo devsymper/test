3a221b4f6d6e25057d870a8ef37a222a47cab748
ADN <anhdungngo1001@gmail.com>
2020-09-04 11:27:41 +0700

    update common upload file cômponents

--- src/views/comment/testcomment.vue
+++ src/views/comment/testcomment.vue
@@ -1,36 +1,8 @@
 <template>
 		<div>
-			<v-tooltip left>
-				<template v-slot:activator="{ on, attrs }">
-					<v-btn icon @click="clickFull($event)" v-bind="attrs"
-						v-on="on"> 
-						<v-icon style="font-size:15px">mdi-comment-multiple</v-icon>
-					</v-btn>
-				</template>
-				<span>test comment by id</span>
-			</v-tooltip>
-			<v-tooltip left>
-				<template v-slot:activator="{ on, attrs }">
-					<v-btn icon @click="clickArea($event)" v-bind="attrs"
-						v-on="on"> 
-					<v-icon style="font-size:15px">mdi-comment-multiple-outline</v-icon>
-				</v-btn>
-				</template> 
-				<span>test comment by uuid</span>
-			</v-tooltip>
-					<comment 
-						:showComment="showComment" 
-						:heightComment="heightComment" 
-						:top="top" 
-						:left="left"
-						:objectIdentifier="idItem"
-						:uuid="uuidArea"
-						:targetArea="contentTargetArea"
-						:objectType="objType"
-					/>
-				<UploadAvatar />
-				<UploadFile  />
-		</div>
+				<UploadFile :autoUpload="true" :pickAvatar="true" @selected-file="selectedFile" :fileName="'adn100198'"/>
+				<img :src="src" alt="">
+		</div>	
 </template>
 
 <script>
@@ -45,22 +17,10 @@ export default {
 		UploadFile
 	},
 	methods:{
-		clickFull(event){
-			this.showComment = !this.showComment;
-			this.heightComment = "800px"
-			this.idItem = 1721
-			this.objType = 'document'
-			this.contentTargetArea = ''
-			this.uuidArea = "0"
-		},
-		clickArea(event){
-			this.showComment = !this.showComment;
-			this.heightComment ='200px'
-			this.idItem = 1721
-			this.objType = 'document'
-			this.contentTargetArea = '<span>ahihihi</span>'
-			this.uuidArea = 'asdasdasdasd'
-		},
+		selectedFile(data){
+			this.src = data
+		}
+	
 	},
 	 data: function() {
         return {
@@ -74,6 +34,7 @@ export default {
 			uuidArea:'0',
 			contentTargetArea:'',
 			objType:'',
+			src: '',
 			commentTarget:{}
         };
     }
