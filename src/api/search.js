cf222b4fcbcdccc19581b4bc946fccc805063faa
hoangquynhanhh <qaht@symper.vn>
2020-10-01 06:41:03 +0700

    sửa bug hiển thị tên + icon

--- src/api/search.js
+++ src/api/search.js
@@ -2,6 +2,7 @@ import Api from "./api";
 import { appConfigs } from "./../configs.js";
 
 let api = new Api(appConfigs.apiDomain.search);
+let apiSyql = new Api(appConfigs.apiDomain.formulasService);
 
 export default {
     getData(value) {
@@ -9,7 +10,7 @@ export default {
             keyword: value
         })
     },
-    getInfoSyql(id) {	
-        return apiSyql.get('formulas/' + id + '/source')	
+    getInfoSyql(id) {
+        return apiSyql.get('formulas/' + id + '/source')
     }
 }
