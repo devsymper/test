53970bc574c0408c034fcac94c7570523d21407d
khadmsymper <khadm@symper.vn>
2020-10-01 00:38:50 +0700

    fix bug my item

--- src/views/process/RunInstance.vue
+++ src/views/process/RunInstance.vue
@@ -79,23 +79,46 @@ export default {
                 try {
                     let instanceName = await this.getInstanceName([]);
                     let newProcessInstance = await runProcessDefinition(this, processDef, [], instanceName);
-                    let filter={};
-                    filter.processInstanceId=newProcessInstance.id;
-                    let dataTaskNew=await BPMNEApi.getTask(filter);
-                    if (dataTaskNew.total>0) {
-                        let arrTask=dataTaskNew.data;
-                        arrTask.forEach(task => {
-                            if (task.assignee==self.$store.state.app.endUserInfo.id) {
-                                self.$router.push("/myitem/tasks/"+task.id);
-                            }
-                        });
-                    }
+                    this.checkAndGotoMyTask(newProcessInstance);
                     this.$snotifySuccess("Workfow started successfully!");
                 } catch (error) {
                     this.$snotifyError(error ,"Error on run process definition ");
                 }
             }
             this.startWorkflowStatus = 'started';
+        },
+        async checkAndGotoMyTask(newProcessInstance){
+            let filter={};
+            let arrTask = [];
+            filter.processInstanceId = newProcessInstance.id;
+            let dataTaskNew = await BPMNEApi.getTask(filter); // lấy task theo quy trình hiện tại
+            if (dataTaskNew.total>0) {
+                arrTask = dataTaskNew.data;
+            }else { // lấy task theo quy trình con 
+                let childProcessInstances = await BPMNEApi.getProcessInstance({
+                    superProcessInstanceId: newProcessInstance.id
+                });
+                if(childProcessInstances.data.length > 0){
+                    let myTasks = [];
+                    for(let instance of childProcessInstances.data){
+                        myTasks.push(
+                            BPMNEApi.getTask({
+                                processInstanceId: instance.id,
+                            })
+                        ); 
+                    }
+                    myTasks = await Promise.all(myTasks);
+                    for(let res of myTasks){
+                        arrTask = arrTask.concat(res.data);
+                    }
+                }
+            }
+
+            arrTask.forEach(task => {
+                if (task.assignee == this.$store.state.app.endUserInfo.id) {
+                    this.$router.push("/myitem/tasks/"+task.id);
+                }
+            });
         },
         getStartDocId(definitionModel){
             return Number(definitionModel.mainProcess.initialFlowElement.formKey);
@@ -123,7 +146,6 @@ export default {
                 dataInputForFormula = varsForBackend.nameAndValueMap;
                 
                 let instanceName = await this.getInstanceName(dataInputForFormula);
-                
                 let newProcessInstance = await runProcessDefinition(this, processDef, vars, instanceName);
                 this.$snotifySuccess("Task submited successfully");
                 this.$router.push('/documents/objects/'+outcomeData.document_object_id);
@@ -148,21 +170,11 @@ export default {
                     resolve('');
                 }else{
                     if(dataObjsMap.instanceDisplayText){
-                        formulasApi.execute({
-                            data_input: JSON.stringify(dataInput),
-                            formula: dataObjsMap.instanceDisplayText.value
-                        }).then((formulaData) => {
-                            if( formulaData.status == 200){
-                                formulaData = formulaData.data.data;
-                                if(formulaData.length > 0){
-                                    formulaData = formulaData[0];
-                                    resolve(Object.values(formulaData)[0]);
-                                }else{
-                                    resolve('');
-                                }
-                            }else{
-                                resolve('');
-                            }
+                        formulasApi.getDataByAllScriptType(
+                            dataObjsMap.instanceDisplayText.value, 
+                            JSON.stringify(dataInput)
+                        ).then((formulaData) => {
+                            resolve(formulaData);
                         }).catch(err=>{
                             reject(err);
                         });                    
