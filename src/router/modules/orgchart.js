f676da22fdd3b68fff12f504706201f2591e4bb3
anhdungngo <dungna@symper.vn>
2020-10-05 11:04:15 +0700

    dung them tinh nang clone orgchart

--- src/router/modules/orgchart.js
+++ src/router/modules/orgchart.js
@@ -22,5 +22,11 @@ export default [{
         name: "viewOrgchart",
         component: () =>
             import ('../../views/orgchart/ViewOrgchart.vue'),
+    },
+    {
+        path: "/orgchart/:id/clone",
+        name: "cloneOrgchart",
+        component: () =>
+            import ('../../views/orgchart/CloneOrgchart.vue'),
     }
 ]
