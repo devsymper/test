af7d49d6e64e392c54a990097500332e4eef631d
Anh Tger <anhnt@symper.vn>
2020-10-28 17:03:47 +0700

    Anh commit daoj lan 2

--- src/components/kh/KHSideBarRight.vue
+++ src/components/kh/KHSideBarRight.vue
@@ -509,7 +509,6 @@ export default {
             $("#toc").toc({
                 content: "#myeditablediv",
                 headings: "h1,h2,h3,h4,h5,h6",
-                hash:this.hash
             });
         },
         getData(hash = false) {
