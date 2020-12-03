c10a93ffee8a4d349e29bf3ff3c2a55d1161fc69
Anh Tger <anhnt@symper.vn>
2020-12-03 16:38:17 +0700

    update

--- src/components/taskManagement/version/DetailVersion.vue
+++ src/components/taskManagement/version/DetailVersion.vue
@@ -1,6 +1,20 @@
 <template>
-    <div class="w-100 h-100" >
-        <h2 class="ml-4">Version {{infoVersion.name}}</h2>
+    <div class="w-100 h-100" v-if="Object.keys(infoVersion).length >0 " >
+        <div class="d-flex" style="height:40px">
+            <h2 class="ml-4">Version {{infoVersion.name}}</h2>
+            <span class="ml-2">
+                <v-chip class="px-2" style="border-radius:4px" v-if="infoVersion.status == 0" color="#0760D9" text-color="white" x-small>{{$t('taskManagement.table.unreleased')}}</v-chip>
+                <v-chip class="px-2" style="border-radius:4px" v-else-if="infoVersion.status == 1" color="#408137" text-color="white" x-small>{{$t('taskManagement.table.released')}}</v-chip>
+            </span>
+        </div>
+        <div class="fs-13 ml-4 grey--text">
+            <v-icon class="fs-13 mr-2">mdi-calendar</v-icon>
+            <span>{{$t("taskManagement.table.startDate")}}:</span>
+            <span class="mx-1">{{infoVersion.startTime}}</span>
+            <span class="mr-1 ml-2">{{$t("taskManagement.table.releaseDate")}}:</span>
+            <span>{{infoVersion.endTime}}</span>
+            
+        </div>
     </div>
 </template>
 
