aeb9be8008d77441722375ee8a4e86ce80c1b8f8
nguyen dinh hoang <hoang.ptit.1998@gmail.com>
2020-12-04 15:56:29 +0700

    thêm compon editor dùng chung

--- src/components/common/FormTpl.vue
+++ src/components/common/FormTpl.vue
@@ -176,6 +176,7 @@ import LinkConfig from "./../../views/document/sideright/items/LinkConfig.vue";
 import FormAutoComplete from "./../../views/document/sideright/items/FormAutoComplete";
 import FormulaEditor from "./../formula/editor/FormulaEditor";
 import DateFormat from "./../common/DateFormat";
+import Editor from "./../common/editor/Editor";
 import NumberFormat from "./../common/NumberFormat";
 import DataTable from "./../common/customTable/DataTable";
 import SymperDragPanel from "./SymperDragPanel";
@@ -365,6 +366,14 @@ const inputTypeConfigs = {
     },
     configTime:{
         tag: "configTime",
+        props(config) {
+            return {
+                label: config.title,
+            };
+        }
+    },
+    editor:{
+        tag: "symper-editor",
         props(config) {
             return {
                 label: config.title,
@@ -696,6 +705,7 @@ export default {
         "datetime-picker" : DateTimePicker,
         SymperColorPicker: SymperColorPicker,
         "default-control-document":SymperDefaultControlDocument,
+        'symper-editor':Editor
 
 
     }
