e9b1b77e40dcd0108a7745dad014e218b6de7b67
ADN <anhdungngo1001@gmail.com>
2020-12-23 11:34:33 +0700

    làm lịch sử task - id RIW083

--- src/components/myItem/taskLifeCycle/HistoryTask.vue
+++ src/components/myItem/taskLifeCycle/HistoryTask.vue
@@ -2,8 +2,8 @@
   <div class="d-flex flex-column mt-2">
 	  <div class="d-flex flex-column mb-2 pt-1 mr-1 ml-1 " v-for="(item,i) in allHistory" :key="i">
 		  <div class="d-flex align-center"> 
-			  <v-icon small>mdi-account</v-icon>
-			 <span class="flex-grow-1">
+			  <v-icon small> {{ item.userCreateActionType == 'owner' ? 'mdi-account-tie-outline' : 'mdi-account-check-outline'  }}</v-icon>
+			 <span class="flex-grow-1  ml-1">
 				  {{item.userCreateAction}}
 			 </span>
 			  <span>
@@ -27,9 +27,9 @@
 					{{taskStatus[item.startStatus].title}}
 				</span>
 			</v-chip>
-			<span>
-				->
-			</span>
+			<v-icon x-small>
+				mdi-chevron-triple-right
+			</v-icon>
 			<v-chip
 					x-small
 					label
@@ -70,7 +70,7 @@ export default {
 				for(let i = 0; i < data.length - 1 ; i++){
 					if(data[i].delegationState == 'PENDING'){
 						if(data[i+1].delegationState == "RESOLVED"){
-							let obj = self.createHistoryTask(data[i],data[i+1],"delegate","assign")
+							let obj = self.createHistoryTask(data[i],data[i+1],"delegate","assign",'assigne')
 							obj.userCreateAction  = mapIdToUser[data[i].assignee].displayName
 							let arr = data[i+1].assignee.split(":")
 							obj.userReceiveAction  = mapIdToUser[arr[0]].displayName
@@ -79,35 +79,35 @@ export default {
 					}
 					else if(data[i].delegationState == 'RESOLVED'){
 						if(data[i+1].delegationState == "PENDING"){
-							let obj = self.createHistoryTask(data[i],data[i+1],"assign","delegate")
+							let obj = self.createHistoryTask(data[i],data[i+1],"assign","delegate",'owner')
 							let arr = data[i].assignee.split(":")
 							obj.userCreateAction  = mapIdToUser[arr[0]].displayName
 							obj.userReceiveAction  = mapIdToUser[data[i+1].assignee].displayName
 							newData.push(obj)
 						}else if(data[i+1].deleted){
-							let obj = self.createHistoryTask(data[i],data[i+1],"assign","complete")
+							let obj = self.createHistoryTask(data[i],data[i+1],"assign","complete",'owner')
 							let arr = data[i].assignee.split(":")
 							obj.userCreateAction  = mapIdToUser[arr[0]].displayName
 							newData.push(obj)
 						}else if(data[i].assignee != data[i+1].assignne){
-							let obj = self.createHistoryTask(data[i],data[i+1],"assign","assign")
+							let obj = self.createHistoryTask(data[i],data[i+1],"assign","assign",'owner')
 							let arr = data[i].assignee.split(":")
 							obj.userCreateAction  = mapIdToUser[arr[0]].displayName
 							obj.userReceiveAction  = mapIdToUser[data[i+1].assignee].displayName
 						}else if(!data[i+1].assignee && !data[i+1].owner){
-							let obj = self.createHistoryTask(data[i],data[i+1],"assign","unAssign")
+							let obj = self.createHistoryTask(data[i],data[i+1],"assign","unAssign",'owner')
 							let arr = data[i].assignee.split(":")
 							obj.userCreateAction  = mapIdToUser[arr[0]].displayName
 						}
 					}
 					else if(!data[i].deleted && data[i+1].deleted){
-						let obj = self.createHistoryTask(data[i],data[i+1],"assign","delegate")
+						let obj = self.createHistoryTask(data[i],data[i+1],"assign","delegate",'owner')
 						let arr = data[i].assignee.split(":")
 						obj.userCreateAction  = mapIdToUser[arr[0]].displayName
 						obj.userReceiveAction  = mapIdToUser[data[i+1].assignee].displayName
 						newData.push(obj)
 					}else if(!data[i].owner && ! data[i].assignee && data[i+1].assignee && data[i+1].owner){
-						let obj = self.createHistoryTask(data[i],data[i+1],"unAssign","assign")
+						let obj = self.createHistoryTask(data[i],data[i+1],"unAssign","assign",'assigne')
 						let arr = data[i].assignee.split(":")
 						obj.userCreateAction  = mapIdToUser[arr[0]].displayName
 						obj.userReceiveAction  = mapIdToUser[data[i+1].assignee].displayName
@@ -164,12 +164,14 @@ export default {
 				return "Hoàn thành công việc"
 			}
 		},
-		createHistoryTask(x,y,startStatus,endStatus){
+		createHistoryTask(x,y,startStatus,endStatus,userCreateActionType){
 			let self = this
 			let obj = {}
 			obj.startStatus = startStatus
 			obj.endStatus = endStatus
 			obj.date = self.$moment(y.createTime).format('YYYY-MM-DD')
+			obj.userCreateActionType  = userCreateActionType
+
 			return obj
 		}
 	}
