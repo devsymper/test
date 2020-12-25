6722f5bb594f4bac633f6ea77121c327d8f24866
ADN <anhdungngo1001@gmail.com>
2020-12-25 10:18:45 +0700

    chuyển phần config app

--- src/worker/application/Application.Worker.js
+++ src/worker/application/Application.Worker.js
@@ -2,6 +2,7 @@ import {documentApi} from '@/api/Document';
 import {orgchartApi} from '@/api/orgchart';
 import {dashboardApi} from '@/api/dashboard';
 import BpmnEngine from '@/api/BPMNEngine';
+import {appManagementApi} from '@/api/AppManagement.js';
 
 
 self.onmessage = async function (event) {
@@ -12,6 +13,15 @@ self.onmessage = async function (event) {
         case 'search':
 			let searchData = await searchItem(data);
             postMessage({action:'search', dataAfter : searchData})
+            break;
+        case 'updateApp':
+			let updateAppData = await updateApp(data);
+			debugger
+            postMessage({action:'search', dataAfter : updateAppData})
+            break;
+        case 'createApp':
+			let createAppData = await createApp(data);
+            postMessage({action:'search', dataAfter : createAppData})
             break;
        
         default:
@@ -24,7 +34,6 @@ export const searchItem = async function(data){
 	let obj = {}
 	let orgchartData = await orgchartApi.getOrgchartList({search: data.value, pageSize:50})
 	obj.orgchart = orgchartData.data.listObject;
-
 	let documnentData = await documentApi.searchListDocuments({search: data.value, pageSize:100})
 	let arrCategory = []
 	let arrMajor = []
@@ -44,4 +53,52 @@ export const searchItem = async function(data){
 	let dashboardData = await dashboardApi.getDashboardsApp({search: data.value, pageSize:50})
 	obj.dashboard = dashboardData.data.listObject;
 	return obj
+}
+export const updateListItem = function(listItemSelected, childrenAppData , currentAppData){
+	for(let i in childrenAppData){
+		childrenAppData[i] = []
+	}
+	if(listItemSelected.document_category.item.length > 0){
+		listItemSelected.document_category.item.forEach(function(e){
+			childrenAppData.document_definition.push(e.id);
+		});
+	}
+	if(listItemSelected.document_major.item.length > 0){
+		listItemSelected.document_major.item.forEach(function(e){
+			childrenAppData.document_definition.push(e.id);
+		});
+	}
+	if(listItemSelected.orgchart.item.length > 0){
+		listItemSelected.orgchart.item.forEach(function(e){
+		childrenAppData.orgchart.push(e.id);
+	});
+	}
+	if(listItemSelected.dashboard.item.length > 0){
+		listItemSelected.dashboard.item.forEach(function(e){
+		childrenAppData.dashboard.push(e.id);
+	});
+	}
+	if(listItemSelected.workflow_definition.item.length > 0){
+		listItemSelected.workflow_definition.item.forEach(function(e){
+			childrenAppData.workflow_definition.push(e.id);
+		});
+	}
+	currentAppData.childrenApp =  childrenAppData
+	return currentAppData
+}
+export const updateApp = async function(data){
+	let dataApp = JSON.stringify(updateListItem(data.listItemSelected, data.childrenAppData, data.currentAppData))
+	let res =  await appManagementApi.updateApp(dataApp)
+	debugger
+	if(res.status == 200){
+		return 'success'
+	}else{
+		return 'false'
+	}
+}
+export const createApp = async function(data){
+	let dataApp = JSON.stringify(updateListItem(data.listItemSelected, data.childrenAppData, data.currentAppData))
+	debugger
+	let res =  await appManagementApi.addApp(dataApp)
+	return res
 }
