541e11e117440a25228617a10fb6b8916db87267
khadmSymper <khadm@symper.vn>
2020-12-28 14:11:19 +0700

    thêm phần đưa login info xuống worker để chạy được api gọi từ worker

--- src/plugins/utilModules/auth.js
+++ src/plugins/utilModules/auth.js
@@ -2,6 +2,20 @@
 /**
  * Các hàm phục vụ cho việc xác thực, lưu trữ dữ liệu đăng nhập cho người dùng
  */
+import IndexedDB from "@/plugins/utilModules/indexedDB.js";
+var loginedInfo = {};
+if(!self.document){// Nếu nằm dưới worker
+    function getAndSetLoginInfoForWorker() {
+        let indexedDB = new IndexedDB('SYMPER-LOGIN-INFOR');
+        indexedDB.open('loginInfo', false, false, async()=>{
+            loginedInfo = await indexedDB.read('loginInfo');
+            if(typeof loginedInfo == 'string'){
+                loginedInfo = JSON.parse(loginedInfo);
+            }
+        });
+    }
+    getAndSetLoginInfoForWorker();
+}
 export const authUtil = {
 	saveLoginInfo(data) {
         /**
@@ -23,7 +37,7 @@ export const authUtil = {
 			    return false;
 			}
 		}else{
-			return "eyJhbGciOiJSUzI1NiIsInR5cGUiOiJKV1QifQ==.eyJpZCI6IjIiLCJuYW1lIjoiTmd1eVx1MWVjNW4gVmlcdTFlYzd0IERpbmgiLCJlbWFpbCI6ImRpbmhudkBzeW1wZXIudm4iLCJ1c2VyRGVsZWdhdGUiOnsiaWQiOiI5MjIiLCJmaXJzdE5hbWUiOiJcdTAxMTBpXHUxZWM3cCIsImxhc3ROYW1lIjoiIiwidXNlck5hbWUiOiJcdTAxMTBpXHUxZWM3cCBUQktTIFx1MDExMFx1MDBlMG0gVGhcdTFlY2IiLCJkaXNwbGF5TmFtZSI6Ilx1MDExMGlcdTFlYzdwIFRCS1MgXHUwMTEwXHUwMGUwbSBUaFx1MWVjYiIsImVtYWlsIjoidGh1eWRpZXBAcGh1Y2hhLmNvbSIsInBob25lIjoidGVzdCBiaW5kIGxhc3QzIiwic3RhdHVzIjoiMSIsImF2YXRhciI6IiIsInR5cGUiOiJ1c2VyIiwiaXAiOiIxNC4xNjIuMTcxLjEzOSIsInVzZXJBZ2VudCI6Ik1vemlsbGFcLzUuMCAoV2luZG93cyBOVCAxMC4wOyBXaW42NDsgeDY0KSBBcHBsZVdlYktpdFwvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lXC84Ny4wLjQyODAuODggU2FmYXJpXC81MzcuMzYiLCJsb2NhdGlvbiI6W10sInJvbGUiOiJvcmdjaGFydDoxMDk6YmU3YTZlM2EtMWI2YS00MGU2LTgwNTctODg5ZDYyZTMzMjg4In0sInR5cGUiOiJiYSJ9.NDNiMzQ0NzE1ODAyZGFjODg2ZWY0MjFmNmQ2YWI0NTNhY2U2ZThlMzIwYWI3MGQ4MTEwY2JlODg3MzliNjBkYjg0Mzk3NmQxMGI1ZjQ0NDQ5OGZiZWNkZmVlOGRkMjk1MzkyODRhMmZmOWRlZWUyYWFmZGNlNWYyNGYxYzE5ZGJlNjNmYWE2YjYyNGJlNGZmNzhlNzcwNDE3ZjZkMjUwNjZkYjcwOTllNjZlZTNmNWFlOWVhZGE5MzM3ZGI0ZDhjZWUxOWM3YjBiNmU3ZDBkOGFkYWZkYzBlOTZjMGQ2NjY5MDAyMDczZTg5NzA5N2JhODZiNmY3MjVlNTYwYTg5YWI5YzJkZjAyODc0ODExOTE0MGFkMDgzZGMwN2QxZDBjYzFkNjU4OTVlMzU1MjIyODc2NGY3YTI5MjAzNWM3MzUxNmQ3MGUzMDNiODVmMzIzMzM1ZjRmNTk0YzE3ZDBlYTgxMzk0OGYxMTViZjU5NGM1MjI3MGU4NmMzZWJmZWZiYjAxNmM5OTAxZTJkNDY1Mjk0NjEyODFmNTc4MWFiN2JhY2MyZTVkMTQ4ZDE0NDM2Zjk0M2FjYjcwOGFjMzE1Y2IzZTBlZmQyNjBiZTRhMmIxZGQzNGI1N2U0ZDA1NThmNTIyMTFkNjdlYjg1MjJkODA3MTU3ZjA1ZDY1YjNjMGY="
+            return loginedInfo.token;
 		}
     },
 
@@ -52,7 +66,6 @@ export const authUtil = {
     },
 
     setSavedUserInfo(data) {
-
         let loginInfo = this.getSavedUserInfo();
         loginInfo = Object.assign(loginInfo, data);
         this.saveLoginInfo(loginInfo);
@@ -68,6 +81,16 @@ export const authUtil = {
             role = data.profile.role;
         }
         return role;
+    },
+
+    checkLoginAndSetToIndexedDB(){
+        if(this.checkLogin()){
+            let logiInfo = this.getSavedUserInfo();
+            let indexedDB = new IndexedDB('SYMPER-LOGIN-INFOR');
+            indexedDB.open('loginInfo', false, false, ()=>{
+                indexedDB.save(logiInfo, 'loginInfo');
+            });
+        }
     }
 
 }
