3625b9ca30b179edcc8e0d1de32aa406556ad297
nguyen dinh hoang <hoang.ptit.1998@gmail.com>
2021-01-14 10:36:02 +0700

    thêm report

--- src/views/taskManagement/report/Report.vue
+++ src/views/taskManagement/report/Report.vue
@@ -1,17 +1,18 @@
 <template>
+    <dashboard-view :url="'https://dev.bi.symper.vn/#/dashboard/159/view'">
+
+    </dashboard-view>
 </template>
 <script>
+import DashboardView from '../../dashboard/DashboardView.vue';
 
 export default {
     name: "Report",
     components:{
+        DashboardView
     },
     methods:{
         
     },
-    activated(){
-        //this.toggleMainContentLoader(false);
-    }
-    
 };
 </script>
