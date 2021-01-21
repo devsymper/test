00c0276221c14ef8fdcf16e454db9d0cd97ac974
Anh Tger <anhnt@symper.vn>
2021-01-21 13:40:08 +0700

    pull code bi

--- src/components/dashboard/reports/ReportLine.chart.js
+++ src/components/dashboard/reports/ReportLine.chart.js
@@ -2,7 +2,7 @@ import ReportGroupConfig from '@/components/dashboard/configPool/reportGroupConf
 import ReportBase from './ReportBase'
 import {TranslatorHelper} from '@/components/dashboard/configPool/translatorHelper'
 
-export default class Line extends ReportBase {
+export default class ReportLine extends ReportBase {
     constructor(symperId){
         let columnSettingKeys = ReportGroupConfig.Group2.columnSettingKeys
         let styleKeys = ReportGroupConfig.Group2.styleKeys
