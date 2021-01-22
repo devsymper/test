fdfc628f9fb87bba41ecb8ff9a22603e3a96d39c
khadmSymper <khadm@symper.vn>
2021-01-22 14:52:42 +0700

    fix bug do merge code

--- src/components/dashboard/components/DashboardCell.vue
+++ src/components/dashboard/components/DashboardCell.vue
@@ -6,7 +6,7 @@
             <img :src="'img/dashboard/report-builder/'+cellConfigs.sharedConfigs.type+'.png'" height="40px" width="40px">
         </div>
         <div class="h-100 w-100 symper-dashboard-cell" v-else>
-            <div ref="cellTitle" class="symper-cell-title  pb-1 pt-1" :symper-cell-id="cellConfigs.sharedConfigs.cellId" :style="viewAttrs.symperTitle.style">
+            <div ref="cellTitle" class="symper-cell-title  py-1" :symper-cell-id="cellConfigs.sharedConfigs.cellId" :style="viewAttrs.symperTitle.style">
                 <div class="cell-title-text text-ellipsis  d-inline-block pl-2 pr-2" style="width: calc(100% - 10px)">
                     <span style="cursor: text" v-if="isView">{{viewAttrs.symperTitle.text}}</span>
                     <span 
@@ -58,6 +58,17 @@ export default {
         selectThisCell(){
             this.$store.commit('dashboard/setSelectedCell', {id: this.cellConfigs.sharedConfigs.cellId, instanceKey: this.instanceKey});
         },
+        applyTitleChange(){
+            this.edditingTitle = false;
+            let titleConfig = null;
+            this.cellConfigs.rawConfigs.style.title.children.titleText.value = this.viewAttrs.symperTitle.text;
+        },
+        editTitleCell(){
+            this.edditingTitle = true;
+            setTimeout((self) => {
+                $(self.$refs.renameTitleInput).focus();            
+            }, 0, this);
+        }
     },
     computed: {
         reportTag(){
@@ -67,6 +78,9 @@ export default {
             }else{
                 return null;
             }
+        },
+        viewAttrs(){
+            return this.cellConfigs.viewConfigs.displayOptions;
         }
     },
     props: {
@@ -83,6 +97,9 @@ export default {
         instanceKey: {
             defaul: ''
         },
+        isView: {
+            default: true
+        }
     }
 }
 </script>
