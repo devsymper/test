eccfd3ba853c360ea21983dcf9f5ef25b550181a
hoangquynhanhh <qaht@symper.vn>
2021-01-23 15:48:24 +0700

    Authen tính năng

---
+++ src/components/authen/detailKeyServer.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="fs-16 fw-430 pb-1">
+            {{$t('authen.view.view_title')}}
+        </div>
+        <v-divider class="mb-3">
+        </v-divider>
+        <v-row v-for="(key,idx) in detailKey" :key="idx" >
+            <v-col class="col-md-5 fs-13">
+                 {{$t("authen.table."+idx)}}: 
+            </v-col>
+            <v-col class="col-md-7 fs-13">
+                {{key}}
+            </v-col>
+        </v-row>
+    </div>
+</template>
+<script>
+export default {
+  created () {
+        debugger
+      let arr = Object.keys(this.detailKey);
+      arr.pop();
+      let detailKey = [];
+      arr.map(a=>{
+          detailKey.push({
+              name:a,
+              value:this.detailKey[a]
+          })
+      })
+      debugger
+      this.detailKey = detailKey
+  },
+  props: {
+      detailKey:{
+          type:Object,
+          default:{}
+      }
+  },
+    
+}
+</script>
---
+++ src/views/authen/index.vue
@@ -0,0 +1,173 @@
+<template>
+<div class="w-100">
+     <list-items
+        ref="listServerKey"
+        :showImportHistory="false"
+        :useDefaultContext="false"
+         @after-open-add-panel="addKey"
+        :headerPrefixKeypath="'authen.table'"
+        :pageTitle="$t('authen.table.title')"
+        :containerHeight="containerHeight"
+        :showExportButton="false"
+        :actionPanelWidth="actionPanelWidth"
+        :customAPIResult="customAPIResult"
+        :tableContextMenu="tableContextMenu"
+        :getDataUrl="getListUrl">
+         <div slot="right-panel-content" class="h-100">
+            <add-key 
+                v-show="showAddView"
+                :typeView="typeView"
+                :detailKey="detailKey"
+                @close-panel="close()"
+                ref="addView" 
+                />
+            <detail-key 
+                v-show="showDetailView"
+                :detailKey="detailKey"
+                ref="detailView" 
+                />
+        </div>
+    </list-items>
+    </div>
+</template>
+<script>
+import ListItems from "./../../components/common/ListItems.vue";
+import AddKeyServer from "./../../components/authen/addKeyServer.vue";
+import DetailKeyServer from "./../../components/authen/detailKeyServer.vue";
+import { util } from "./../../plugins/util.js";
+import { appConfigs } from '../../configs';
+import {accessControlApi} from './../../api/accessControl';
+export default {
+    components: {
+        "list-items": ListItems,
+        "add-key":AddKeyServer,
+        "detail-key":DetailKeyServer
+    },
+    data(){
+        const self = this
+        return {
+            detailKey:{},
+            showDetailView:false,
+            listUser:[],
+            showAddView:false,
+            importInfo:{},
+            typeView:"add",
+            customAPIResult: {
+                reformatData(res){
+                    let data =  {
+                         columns:[
+                            {name: "id", title: "id", type: "numeric"},
+                            {name: "name", title: "name", type: "text"},
+                            {name: "description", title: "description", type: "text"},
+                            {name: "serverKey", title: "serverKey", type: "text"},
+                            {name: "status", title: "status", type: "text"},
+                            {name: "userUpdate", title: "userUpdate", type: "text"},
+                            {name: "userCreate", title: "userCreate", type: "text"},
+                            {name: "createAt", title: "createAt", type: "text"},
+                            {name: "updateAt", title: "updateAt", type: "text"},
+                       ],
+                        listObject:res.data,
+                        total:res.data.length
+                    }
+                     return data
+                } 
+            },
+             tableContextMenu:{
+                view: {
+                    name:"view",
+                    text:this.$t('user.table.contextMenu.view'),
+                    callback: (serverKey, callback) => {
+                        this.viewKey(serverKey);
+                    }
+                },
+                update:{
+                    name:"update",
+                    text:this.$t('user.table.contextMenu.edit'),
+                    callback: (serverKey, callback) => {
+                        this.updateKey(serverKey);
+                    }
+
+                },
+                delete:{
+                    name:"delete",
+                    text:this.$t('user.table.contextMenu.delete'),
+                    callback: (serverKey, callback) => {
+                        this.deleteKey(serverKey);
+                    }
+                }
+            },
+            getListUrl: appConfigs.apiDomain.permission+'server_keys',
+            actionPanelWidth:400,
+            containerHeight: 200,
+            columns: [],
+        }
+    },
+    mounted() {
+        this.calcContainerHeight();
+    },
+    methods:{
+        changeView(addView = false, detailView = true){
+            this.showAddView = addView,
+            this.showDetailView = detailView;
+            this.actionPanelWidth=400;
+        },
+        close(){
+            this.$refs.listServerKey.closeactionPanel();
+            this.$refs.listServerKey.refreshList()
+        },
+        addKey(){
+            this.changeView(true,false);
+            this.typeView = "add";
+        },
+        setKeyValue(){
+            debugger
+            this.$refs.addView.nameKey = this.detailKey.name;
+            this.$refs.addView.descriptionKey = this.detailKey.description;
+            this.$refs.addView.statusKey = this.detailKey.status==1?true:false;
+            this.$refs.addView.keyId = this.detailKey.id;
+        },
+        updateKey(key){
+            this.changeView(true,false);
+            this.typeView = "update";
+            this.$refs.listServerKey.openactionPanel();
+            this.detailKey = key;
+            this.setKeyValue();
+        },
+        getKey(id){
+
+        },
+        viewKey(key){
+            this.changeView(false,true);
+            this.actionPanelWidth=500;
+            this.detailKey = key;
+        },
+        deleteKey(key){
+            key.map(k=>{
+                this.deleteOneKey(k.id)
+            })
+        },
+        deleteOneKey(id){
+             accessControlApi.deleteServerKey(id).then(res=>{
+                if(res.status==200){
+                     this.$snotify({
+                        type: "success",
+                        title: this.$t("authen.view.delete_title")+ this.$t(+"authen.notify.success"),
+                    });
+                this.$refs.listServerKey.refreshList();
+                }else{
+                    this.$snotify({
+                        type: "error",
+                        title: this.$t("authen.view.delete_title")+ this.$t(+"authen.notify.success"),
+                    });
+                }
+            })
+        },
+        refreshListUser(){
+            this.$refs.listUser.refreshList();
+        },
+        calcContainerHeight() {
+            this.containerHeight = util.getComponentSize(this).h;
+        }
+    }
+}
+</script>
